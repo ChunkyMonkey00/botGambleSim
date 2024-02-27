02aa21bbf3fe147b0b9fec3e544483b56f8ad93d
ChunkyMonkey00 <148571454+ChunkyMonkey00@users.noreply.github.com>
2024-02-27 13:39:07 -0500

    Update index.js
    - added a pause option and the ability to see each bot's peak

--- index.js
+++ index.js
@@ -8,21 +8,22 @@ var xxxChance = 20;
 var freq = 20;
 var payCheck = 10;
 var isRunning = false;
+var isPaused = false;
 var inter;
 
 var bots = [
-  [0, 0, 0],
-  [0, 1, 0],
-  [0, 10, 0],
-  [0, 20, 0],
-  [0, 30, 0],
-  [0, 40, 0],
-  [0, 50, 0],
-  [0, 60, 0],
-  [0, 70, 0],
-  [0, 80, 0],
-  [0, 90, 0],
-  [0, 100, 0]
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 10, 0, 0],
+  [0, 20, 0, 0],
+  [0, 30, 0, 0],
+  [0, 40, 0, 0],
+  [0, 50, 0, 0],
+  [0, 60, 0, 0],
+  [0, 70, 0, 0],
+  [0, 80, 0, 0],
+  [0, 90, 0, 0],
+  [0, 100, 0, 0],
 ];
 
 // JavaScript code here
@@ -49,7 +50,7 @@ function addBot(p) {
 
 function gamble(money) {
   let rnd = Math.random() * 100;
-  if(rnd >= xxChance && rnd < xxxChance) {
+  if (rnd >= xxChance && rnd < xxxChance) {
     return 2;
   }
   if (rnd >= xxxChance) {
@@ -63,56 +64,58 @@ function botGamble(who) {
   let percent = bots[who][1];
 
   let moneyCalc = Math.floor((percent / 100) * money);
-  
+
   bots[who][0] -= moneyCalc;
   let turn = gamble(moneyCalc);
-  bots[who][0] += turn*moneyCalc;
+  bots[who][0] += turn * moneyCalc;
 }
 
 function earnMoney(who) {
-  bots[who][0]+= payCheck;
+  bots[who][0] += payCheck;
 }
 var day = 0;
 
 function runSimulation() {
-  day = 0;
-  for(var i =0;i<bots.length;i++) {
-    bots[i][0] = 0;
-  }
-
   xxChance = 100 - xxChance;
   xxxChance = 100 - xxxChance;
 
   inter = setInterval(() => {
-  day++;
-  for(var i =0;i<bots.length;i++) {
-    earnMoney(i);
-    bots[i][2]++;
-    if(bots[i][2] >= freq) {
-      botGamble(i);
-      bots[i][2]=0;
+    day++;
+    for (var i = 0; i < bots.length; i++) {
+      earnMoney(i);
+      bots[i][2]++;
+      if (bots[i][2] >= freq) {
+        botGamble(i);
+        bots[i][2] = 0;
+      }
+      if(bots[i][0] > bots[i][3]) bots[i][3] = bots[i][0];
     }
-  }
-  var formattedBots = bots.map(function (bot, index) {
-    return `BOT${index}: $${bot[0]}; ${bot[1]}%`;
-  });
+    var formattedBots = bots.map(function (bot, index) {
+      return `BOT${index}: $${bot[0]}; ${bot[1]}%; <span style='color: orange;'>Peak: ${bot[3]}</span>`;
+    });
     document.getElementById("game").innerHTML = '<ul>' + formattedBots.map(bot => `<li>${bot}</li>`).join('') + '</ul>';
-}, 1);
+  }, 1);
 
 }
 
 function clearGame() {
   let bestBot = 0;
-  for(var i = 0;i<bots.length;i++) {
-    if(bots[i][0] > bots[bestBot][0]) {
+  for (var i = 0; i < bots.length; i++) {
+    if (bots[i][0] > bots[bestBot][0]) {
       bestBot = i;
     }
   }
-  document.querySelector("#result").innerHTML = `BOT${bestBot} won with ${bots[bestBot][0]}! Percent: ${bots[bestBot][1]}\n Day: ${day}; Gamble: ${Math.floor(day/freq)}`;
+  for (var i = 0; i < bots.length; i++) {
+    bots[i][0] = 0;
+    bots[i][3] = 0;
+  }
+  document.getElementById("game").innerHTML = "";
+  document.querySelector("#result").innerHTML = `BOT${bestBot} won with ${bots[bestBot][0]}! Percent: ${bots[bestBot][1]}\n Day: ${day}; Gamble: ${Math.floor(day / freq)}`;
 
   clearInterval(inter);
-
   day = 0;
+  isPaused = false;
+  document.getElementById("pauseButton").innerText = "Pause";
 }
 
 // Start or stop simulation
@@ -129,5 +132,19 @@ function startStopSimulation() {
     runSimulation();
     document.getElementById("startStopButton").innerText = "Stop";
     isRunning = true;
+  }
+}
+
+function pauseSimulation() {
+  if(isRunning) {
+    if(isPaused) {
+      runSimulation();
+      isPaused = false;
+      document.getElementById("pauseButton").innerText = "Pause";
+    } else {
+    clearInterval(inter);
+    isPaused = true;
+    document.getElementById("pauseButton").innerText = "Play";
+    }
   }
 }
